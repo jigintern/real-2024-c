a937fd3734fa7627df0a5e62494d0cf43ade7f4a
3BLABLAB <rk3b.0215@gmail.com>
2024-08-23 17:09:23 +0900

    first commit
    Qiita APIで記事20個分を作成順にとってくることができる
    ただしレスポンスは成形前

--- server.deno.js
+++ server.deno.js
@@ -4,6 +4,7 @@ Deno.serve(async (req) => {
   const pathname = new URL(req.url).pathname;
   console.log(pathname);
 
+<<<<<<< Updated upstream
   if (req.method === "GET" && pathname === "/article") {
     return new Response({
       title: "title",
@@ -13,6 +14,24 @@ Deno.serve(async (req) => {
       page_views_count: "page_views_count",
       likes_count: "likes_count",
     }, {
+=======
+  //Qiita APIからJSON形式で記事を取得
+  console.log("Qiita API");
+  //トークン情報をヘッダーに登録
+  const reqQiita = new Request(
+    "https://qiita.com/api/v2/items",
+    {
+      headers: {
+        "Authorization": `Bearer ${(Deno.env.get("QIITA_API_TOKEN"))}`,
+      },
+    }
+  );
+  const resQiita = await fetch(reqQiita);
+  console.log(await resQiita.json());
+
+  if (req.method === "GET" && pathname === "/article") {
+    const obj = [{
+>>>>>>> Stashed changes
       title: "title1",
       updated_at: "2000-01-01T00:00:00+00:00",
       url: "https://qiita.com/Qiita/items/c686397e4a0f4f11683d",
@@ -26,6 +45,21 @@ Deno.serve(async (req) => {
       description: "rendered_body2",
       page_views_count: "page_views_count2",
       likes_count: "likes_count2",
+<<<<<<< Updated upstream
+=======
+    }, {
+      title: "title3",
+      updated_at: "2000-01-01T00:00:00+00:00",
+      url: "https://qiita.com/Qiita/items/c686397e4a0f4f11683d",
+      description: "rendered_body2",
+      page_views_count: "page_views_count3",
+      likes_count: "likes_count3",
+    }];
+    return new Response(JSON.stringify(obj), {
+      headers: {
+        "content-type": "text/html",
+      },
+>>>>>>> Stashed changes
     });
   }
 
