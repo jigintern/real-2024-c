59ca94a52ac31ff1a14c65effa7867e4633f62c3
3BLABLAB <rk3b.0215@gmail.com>
2024-08-23 18:03:03 +0900

    Qiita APIの修正
    タイトルだけ抽出

--- server.deno.js
+++ server.deno.js
@@ -4,17 +4,6 @@ Deno.serve(async (req) => {
   const pathname = new URL(req.url).pathname;
   console.log(pathname);
 
-<<<<<<< Updated upstream
-  if (req.method === "GET" && pathname === "/article") {
-    return new Response({
-      title: "title",
-      updated_at: "2000-01-01T00:00:00+00:00",
-      url: "https://qiita.com/Qiita/items/c686397e4a0f4f11683d",
-      description: "rendered_body",
-      page_views_count: "page_views_count",
-      likes_count: "likes_count",
-    }, {
-=======
   //Qiita APIからJSON形式で記事を取得
   console.log("Qiita API");
   //トークン情報をヘッダーに登録
@@ -27,39 +16,23 @@ Deno.serve(async (req) => {
     }
   );
   const resQiita = await fetch(reqQiita);
-  console.log(await resQiita.json());
+  const resQiitaData = await resQiita.json();
+  
+  console.log(
+    resQiitaData.map(item =>{
+      return `title: ${item.title}`
+    }));
 
   if (req.method === "GET" && pathname === "/article") {
-    const obj = [{
->>>>>>> Stashed changes
-      title: "title1",
-      updated_at: "2000-01-01T00:00:00+00:00",
-      url: "https://qiita.com/Qiita/items/c686397e4a0f4f11683d",
-      description: "rendered_body1",
-      page_views_count: "page_views_count1",
-      likes_count: "likes_count1",
-    }, {
-      title: "title2",
-      updated_at: "2000-01-01T00:00:00+00:00",
-      url: "https://qiita.com/Qiita/items/c686397e4a0f4f11683d",
-      description: "rendered_body2",
-      page_views_count: "page_views_count2",
-      likes_count: "likes_count2",
-<<<<<<< Updated upstream
-=======
-    }, {
-      title: "title3",
-      updated_at: "2000-01-01T00:00:00+00:00",
-      url: "https://qiita.com/Qiita/items/c686397e4a0f4f11683d",
-      description: "rendered_body2",
-      page_views_count: "page_views_count3",
-      likes_count: "likes_count3",
-    }];
+    const obj = 
+      resQiitaData.map(item =>{
+        return item.title;
+      })
+    
     return new Response(JSON.stringify(obj), {
       headers: {
         "content-type": "text/html",
       },
->>>>>>> Stashed changes
     });
   }
 
