58d60609c89ab28110b058bb83943b5e9e295542
AreaEffectCloud <areaeffectcloud.256@gmail.com>
2024-08-26 09:30:03 +0900

    サーバから返されるJsonからタイトルと要約を表示させた

--- server.deno.js
+++ server.deno.js
@@ -15,24 +15,49 @@ Deno.serve(async (req) => {
       },
     }
   );
-  const resQiita = await fetch(reqQiita);
-  const resQiitaData = await resQiita.json();
+  // const resQiita = await fetch(reqQiita);
+  // const resQiitaData = await resQiita.json();
   
-  console.log(
-    resQiitaData.map(item =>{
-      return `title: ${item.title}`
-    }));
+  // console.log(
+  //   resQiitaData.map(item =>{
+  //     return `title: ${item.title}`
+  //   }));
 
   if (req.method === "GET" && pathname === "/article") {
-    const obj = 
-      resQiitaData.map(item =>{
-        return item.title;
-      })
+    // const obj = 
+    //   resQiitaData.map(item =>{
+    //     return item.title;
+    //   })  
     
+    // return new Response(JSON.stringify(obj), {
+    //   headers: {
+    //     "content-type": "text/html",
+    //   },
+    // });
+    const obj = [{
+      title: "title",
+      updated_at: "2000-01-01T00:00:00+00:00",
+      url: "https://qiita.com/Qiita/items/c686397e4a0f4f11683d",
+      description: "rendered_body",
+      page_views_count: "page_views_count",
+      likes_count: "likes_count",
+    }, {
+      title: "title1",
+      updated_at: "2000-01-01T00:00:00+00:00",
+      url: "https://qiita.com/Qiita/items/c686397e4a0f4f11683d",
+      description: "rendered_body1",
+      page_views_count: "page_views_count1",
+      likes_count: "likes_count1",
+    }, {
+      title: "title2",
+      updated_at: "2000-01-01T00:00:00+00:00",
+      url: "https://qiita.com/Qiita/items/c686397e4a0f4f11683d",
+      description: "rendered_body2",
+      page_views_count: "page_views_count2",
+      likes_count: "likes_count2",
+    }];
     return new Response(JSON.stringify(obj), {
-      headers: {
-        "content-type": "text/html",
-      },
+      "Content-type": "application/json",
     });
   }
 
