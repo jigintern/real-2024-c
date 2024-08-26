0afd057c76cb48c5e876c3961e6786e7a0ab03df
3BLABLAB <rk3b.0215@gmail.com>
2024-08-26 13:16:42 +0900

    Update: Qiita APIのページ数と検索文字列を可変にしました

--- server.deno.js
+++ server.deno.js
@@ -4,13 +4,19 @@ Deno.serve(async (req) => {
   const pathname = new URL(req.url).pathname;
   console.log(pathname);
 
+  let page = 0;
+
+
   if (req.method === "GET" && pathname === "/article") {
     //Qiita APIからJSON形式で記事を取得
     console.log("Qiita API");
+    const keyword = "Git";
+    
+    page = 1 + (page % 100);
     //トークン情報をヘッダーに登録
     const reqQiita = new Request(
       // created:>=2022-12-01 created:<=2022-12-31で期間を特定
-      "https://qiita.com/api/v2/items?page=1&per_page=20&query=title:Git",
+      `https://qiita.com/api/v2/items?page=${page}&per_page=20&sort=stock&query=title:${keyword}`,
       {
         headers: {
           "Authorization": `Bearer ${(Deno.env.get("QIITA_API_TOKEN"))}`,
