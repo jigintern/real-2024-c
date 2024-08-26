bc0d60a0cc2f8c7735a42662efe504ee4ea425eb
3BLABLAB <rk3b.0215@gmail.com>
2024-08-26 09:42:48 +0900

    WIP: 検索機能は仮実装

--- server.deno.js
+++ server.deno.js
@@ -8,34 +8,34 @@ Deno.serve(async (req) => {
   console.log("Qiita API");
   //トークン情報をヘッダーに登録
   const reqQiita = new Request(
-    "https://qiita.com/api/v2/items",
+    "https://qiita.com/api/v2/items?query=title:Git",
     {
       headers: {
         "Authorization": `Bearer ${(Deno.env.get("QIITA_API_TOKEN"))}`,
       },
-    }
+    },
   );
   const resQiita = await fetch(reqQiita);
   const resQiitaData = await resQiita.json();
-  
+
   console.log(
-    resQiitaData.map(item =>{
-      return `title: ${item.title}`
-    }));
+    resQiitaData.map((item) => {
+      return `title: ${item.title}`;
+    }),
+  );
 
   if (req.method === "GET" && pathname === "/article") {
-    const obj = 
-      resQiitaData.map(item =>{
-        return {
-          title:item.title,
-          updated_at: item.updated_at,
-          url: item.url,
-          descripition: item.rendered_body,
-          page_views_count: page_views_count,
-          likes_count: likes_count
-        }
-      })
-    
+    const obj = resQiitaData.map((item) => {
+      return {
+        title: item.title,
+        updated_at: item.updated_at,
+        url: item.url,
+        descripition: item.rendered_body,
+        page_views_count: item.page_views_count,
+        likes_count: item.likes_count,
+      };
+    });
+
     return new Response(JSON.stringify(obj), {
       headers: {
         "content-type": "application/json",
