685e12ef45b1d491ad01ac1e3430fd1b30dfdd6a
AreaEffectCloud <areaeffectcloud.256@gmail.com>
2024-08-29 11:29:58 +0900

    記事がなくなった場合に再アクセス #48

--- public/script.js
+++ public/script.js
@@ -44,25 +44,27 @@ const genArticle = () => {
     const siteType = getSiteType(url);
     const { from, icon } = siteInfo[siteType] || { icon: 'Unknown', icon: '' };
 
-    card = `<div class="article">
-          <div class="top">
-            <div class="title">${title}&nbsp;...</div>
-            <div class="description"><span id="upload-at">${date}</span> - ${summary}&nbsp;...</div>
-          </div>
-          <div class="bottom">
-            <div class="info">
-              <div class="likes">
-                <i class='bx bxs-heart'></i>
-                <p class="counts">${likes}</p>
-              </div>
-              <div class="comments">
-                <i class='bx bxs-conversation'></i>
-                <p class="counts">${comments}</p>
-              </div>
-            </div>
-            <div class="icon"><img src="${icon}" alt="${from}のアイコン"></div>
-          </div>
-         </div>
+    card = `<a href="${url}" target="_blank">
+                <div class="article">
+                    <div class="top">
+                        <div class="title">${title}&nbsp;...</div>
+                        <div class="description"><span id="upload-at">${date}</span> - ${summary}&nbsp;...</div>
+                    </div>
+                    <div class="bottom">
+                        <div class="info">
+                            <div class="likes">
+                                <i class='bx bxs-heart'></i>
+                                <p class="counts">${likes}</p>
+                            </div>
+                            <div class="comments">
+                                <i class='bx bxs-conversation'></i>
+                                <p class="counts">${comments}</p>
+                            </div>
+                        </div>
+                        <div class="icon"><img src="${icon}" alt="${from}のアイコン"></div>
+                    </div>
+                </div>
+            </a>
             `;
     return card;
 };
@@ -74,10 +76,9 @@ function addNewContent(content, zIndex) {
     newContentElement.classList.add(zIndex);
     newContentElement.innerHTML = content;
     newContentElement.style.zIndex = zIndex;
+    container.appendChild(newContentElement);
 
-    container.appendChild(newContentElement); // 末尾に div 追加
-
-    // 初期位置の設定
+    // 初期位置
     gsap.set(newContentElement, {
         scale: 0.3,
         top: '4%',
@@ -124,17 +125,32 @@ function addNewContent(content, zIndex) {
     });
 };
 
+// 配列をシャッフル
+const shuffleArray = (array) => {
+    return array.slice().sort(() => Math.random() - Math.random());
+}
+
+// ページが読み込まれたとき
 globalThis.onload = async () => {
     const response = await fetch("/article?qiita=true&zenn=true&issou=true");
     const resJson = await response.json();
-    articles = [...resJson.Qiita, ...resJson.Zenn, ...resJson.Issou];
-    // ! 記事をシャッフル !
+    const articlesAll = [...resJson.Qiita, ...resJson.Zenn, ...resJson.Issou];
+    // 記事のシャッフル
+    articles = shuffleArray(articlesAll);
     addNewContent(genArticle(), 999);
 };
 
 let zIndex = 998;
 // 3秒ごとに新しいコンテンツを追加
-setInterval(() => {
+setInterval( async () => {
     addNewContent(genArticle(), zIndex);
     zIndex -= 1;
+    
+    // 記事が無くなった場合の再アクセス
+    if (articles.length <= 5) {
+        const response = await fetch("/article?qiita=true&zenn=true&issou=true");
+        const resJson = await response.json();
+        const articlesAll = [...resJson.Qiita, ...resJson.Zenn, ...resJson.Issou];        
+        articles.unshift(...shuffleArray(articlesAll));
+    }
 }, 7000);
